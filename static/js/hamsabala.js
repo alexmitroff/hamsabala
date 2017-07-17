4effb2594e45b7250668322b7904e12e3b1beee8
alexmitroff <alexmitroff@gmail.com>
2017-07-17 15:51:21 +0300

    Started add instagram, tried some cross origin workarounds

--- static/js/hamsabala.js
+++ static/js/hamsabala.js
@@ -2,3 +2,17 @@ function toggleMenu(){
     document.getElementById("menu").classList.toggle("active");
     document.getElementById("mobile-menu-button").classList.toggle("active");
 }
+
+function getInsta(){
+    var data = {};
+    $.ajax({
+        url:"https://www.instagram.com/hamsabala.pro/media/",
+        jsonp: "callback",
+        dataType: "json",
+        data: data,
+        success: function(response){
+            console.log(responce["items"]);
+            }
+    });
+
+}
