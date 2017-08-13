25cd3ef6863e2c7f0829b48ae79cb1cfcc6e0c10
alexmitroff <alexmitroff@gmail.com>
2017-08-13 19:51:45 +0300

    Normal title area in sections

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,9 @@ from products.models import *
 
 # Register your models here.
 
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ('collection', 'section')
+
 admin.site.register(Section)
 admin.site.register(Collection)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
