d30987a98df5506d69a633466b79ca0266422b09
alexmitroff <alexmitroff@gmail.com>
2017-08-22 14:18:32 +0300

    Fixed section title size on mobile devices

--- hamsabala/settings.py
+++ hamsabala/settings.py
@@ -36,6 +36,11 @@ ADMINS = DJ_ADMINS
 
 # Application definition
 
+INTERNAL_IPS = (
+        '0.0.0.0',
+        '127.0.0.1',
+        )
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
